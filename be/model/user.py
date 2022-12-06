a7f5670c78c8f580bb058240a09ca05fb83ffc3d
saulty-han <90241708+saulty-han@users.noreply.github.com>
2022-12-07 00:19:37 +0800

    Add files via upload

--- be/model/user.py
+++ be/model/user.py
@@ -1,8 +1,13 @@
 import jwt
 import time
 import logging
-import sqlite3 as sqlite
 from model import error
+import base64
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy.exc as SQLAlchemyError
+from init_db.init_database import Users
 from model import db_conn
 
 # encode a json string like:
@@ -32,6 +37,12 @@ def jwt_decode(encoded_token, user_id: str) -> str:
     decoded = jwt.decode(encoded_token, key=user_id, algorithms="HS256")
     return decoded
 
+# class db():
+#     def __init__(self):
+#         engine = create_engine('postgresql://postgres:CJY1111804@localhost:5432/postgres')
+#         Base = declarative_base()
+#         DBSession = sessionmaker(bind=engine)
+#         self.session = DBSession() 
 
 class User(db_conn.DBConn):
     token_lifetime: int = 3600  # 3600 second
@@ -43,11 +54,11 @@ class User(db_conn.DBConn):
         try:
             if db_token != token:
                 return False
-            jwt_text = jwt_decode(encoded_token=token, user_id=user_id)
+            jwt_text = jwt_decode(encoded_token=token, user_id=user_id) # 得到所有token信息
             ts = jwt_text["timestamp"]
-            if ts is not None:
+            if ts is not None: # 时间戳存在
                 now = time.time()
-                if self.token_lifetime > now - ts >= 0:
+                if self.token_lifetime > now - ts >= 0: # 还未超时
                     return True
         except jwt.exceptions.InvalidSignatureError as e:
             logging.error(str(e))
@@ -55,34 +66,31 @@ class User(db_conn.DBConn):
 
     def register(self, user_id: str, password: str):
         try:
-            terminal = "terminal_{}".format(str(time.time()))
-            token = jwt_encode(user_id, terminal)
-            self.conn.execute(
-                "INSERT into user(user_id, password, balance, token, terminal) "
-                "VALUES (?, ?, ?, ?, ?);",
-                (user_id, password, 0, token, terminal), )
-            self.conn.commit()
-        except sqlite.Error:
+            terminal = "terminal_{}".format(str(time.time())) # 终端init
+            token = jwt_encode(user_id, terminal) # token init
+            self.session.execute( 
+                "INSERT into usr(user_id, password, balance, token, terminal) "
+                "VALUES (?, ?, ?, ?, ?);", (user_id, password, 0, token, terminal) ) # 注册用户init
+            self.session.commit()
+        except SQLAlchemyError:
             return error.error_exist_user_id(user_id)
         return 200, "ok"
 
     def check_token(self, user_id: str, token: str) -> (int, str):
-        cursor = self.conn.execute("SELECT token from user where user_id=?", (user_id,))
-        row = cursor.fetchone()
-        if row is None:
+        row = self.session.query(Users).filter(Users.user_id==user_id).first()
+        if row is None: # 查无此人
             return error.error_authorization_fail()
-        db_token = row[0]
-        if not self.__check_token(user_id, db_token, token):
+        db_token = row.token
+        if not self.__check_token(user_id, db_token, token): # token不对
             return error.error_authorization_fail()
         return 200, "ok"
 
     def check_password(self, user_id: str, password: str) -> (int, str):
-        cursor = self.conn.execute("SELECT password from user where user_id=?", (user_id,))
-        row = cursor.fetchone()
-        if row is None:
+        row = self.session.query(Users).filter(Users.user_id==user_id).first()
+        if row is None: # 查无此人
             return error.error_authorization_fail()
 
-        if password != row[0]:
+        if password != row.password: # 密码错误
             return error.error_authorization_fail()
 
         return 200, "ok"
@@ -90,18 +98,20 @@ class User(db_conn.DBConn):
     def login(self, user_id: str, password: str, terminal: str) -> (int, str, str):
         token = ""
         try:
-            code, message = self.check_password(user_id, password)
-            if code != 200:
+            code, message = self.check_password(user_id, password) # 密码没问题
+            if code != 200: # 返回不对
                 return code, message, ""
 
-            token = jwt_encode(user_id, terminal)
-            cursor = self.conn.execute(
-                "UPDATE user set token= ? , terminal = ? where user_id = ?",
-                (token, terminal, user_id), )
-            if cursor.rowcount == 0:
+            token = jwt_encode(user_id, terminal) # 得到token所有信息
+            # cursor = self.conn.execute( 
+            #     "UPDATE user set token= '%s' , terminal = ? where user_id = '%s'" %
+            #     (token, terminal, user_id)) # 更新用户token，terminal
+            cursor=self.session.query(Users).filter(Users.user_id==user_id).first()
+
+            if cursor is None:
                 return error.error_authorization_fail() + ("", )
-            self.conn.commit()
-        except sqlite.Error as e:
+            self.session.commit()
+        except SQLAlchemyError as e:
             return 528, "{}".format(str(e)), ""
         except BaseException as e:
             return 530, "{}".format(str(e)), ""
@@ -109,21 +119,21 @@ class User(db_conn.DBConn):
 
     def logout(self, user_id: str, token: str) -> bool:
         try:
-            code, message = self.check_token(user_id, token)
+            code, message = self.check_token(user_id, token) # 检查token
             if code != 200:
                 return code, message
 
             terminal = "terminal_{}".format(str(time.time()))
             dummy_token = jwt_encode(user_id, terminal)
 
-            cursor = self.conn.execute(
-                "UPDATE user SET token = ?, terminal = ? WHERE user_id=?",
-                (dummy_token, terminal, user_id), )
-            if cursor.rowcount == 0:
+            cursor = self.session.execute(
+                "UPDATE usr SET token = ? WHERE user_id=?", (dummy_token, user_id))
+
+            if cursor is None:
                 return error.error_authorization_fail()
 
-            self.conn.commit()
-        except sqlite.Error as e:
+            self.session.commit()
+        except SQLAlchemyError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
@@ -135,12 +145,12 @@ class User(db_conn.DBConn):
             if code != 200:
                 return code, message
 
-            cursor = self.conn.execute("DELETE from user where user_id=?", (user_id,))
-            if cursor.rowcount == 1:
+            cursor = self.conn.execute("DELETE from user where user_id='%s'" % (user_id))
+            if cursor.rowcount == 1: # 用户存在，且只有一个
                 self.conn.commit()
             else:
                 return error.error_authorization_fail()
-        except sqlite.Error as e:
+        except SQLAlchemyError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
@@ -154,14 +164,14 @@ class User(db_conn.DBConn):
 
             terminal = "terminal_{}".format(str(time.time()))
             token = jwt_encode(user_id, terminal)
-            cursor = self.conn.execute(
-                "UPDATE user set password = ?, token= ? , terminal = ? where user_id = ?",
-                (new_password, token, terminal, user_id), )
-            if cursor.rowcount == 0:
+            cursor = self.session.execute(
+                "UPDATE user set password = ?, token= ? , terminal = ? where user_id = ?", 
+                (new_password, token, terminal, user_id)) # 更新密码
+            if cursor is None:
                 return error.error_authorization_fail()
 
-            self.conn.commit()
-        except sqlite.Error as e:
+            self.session.commit()
+        except SQLAlchemyError as e:
             return 528, "{}".format(str(e))
         except BaseException as e:
             return 530, "{}".format(str(e))
