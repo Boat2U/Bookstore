3cab7d32ce8cb494be657177cac7655393e09439
Boat <88428187+Boat2U@users.noreply.github.com>
2022-12-07 00:57:30 +0800

    Add files via upload

--- be/model/db_conn.py
+++ be/model/db_conn.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 class DBConn:
     def __init__(self):
-        self.engine = create_engine('postgresql://postgres:xxx@localhost:5432/bookstore')
+        self.engine = create_engine('postgresql://postgres:860514@localhost:5432/bookstore')
         self.Base = declarative_base()
         self.metadata = MetaData()
         self.DBSession = sessionmaker(bind = self.engine)
@@ -21,7 +21,7 @@ class DBConn:
         else:
             return True
 
-    def book_id_exist(self, store_id, book_id):
+    def book_id_exist(self, book_id):
         cursor = self.session.execute("SELECT book_id FROM store WHERE book_id = %d;"% (int(book_id),))
         row = cursor.fetchone()
         if row is None:
@@ -29,10 +29,10 @@ class DBConn:
         else:
             return True
 
-#     def store_id_exist(self, store_id):
-#         cursor = self.session.execute("SELECT store_id FROM user_store WHERE store_id = '%s';"% (store_id,))
-#         row = cursor.fetchone()
-#         if row is None:
-#             return False
-#         else:
-#             return True
+    # def store_id_exist(self, store_id):
+    #     cursor = self.session.execute("SELECT store_id FROM user_store WHERE store_id = '%s';"% (store_id,))
+    #     row = cursor.fetchone()
+    #     if row is None:
+    #         return False
+    #     else:
+    #         return True
