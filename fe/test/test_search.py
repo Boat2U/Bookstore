9cc3895abbf588c474d2fd08d81fcc0f0680b800
saulty-han <90241708+saulty-han@users.noreply.github.com>
2022-12-08 20:48:32 +0800

    Add files via upload

--- fe/test/test_search.py
+++ fe/test/test_search.py
@@ -11,72 +11,67 @@ class TestSearch:
     @pytest.fixture(autouse=True)
     def pre_run_initialization(self):
         self.auth = auth.Auth(conf.URL)
-        self.title = "test_title_{}".format(str(uuid.uuid1()))#random.choice(["很","在"])
+        self.title = "test_title_{}".format(str(uuid.uuid1()))
         self.publisher = "test_publisher_{}".format(str(uuid.uuid1()))
         self.isbn = "test_isbn_{}".format(str(uuid.uuid1()))
         self.book_intro = "test_book_intro_{}".format(str(uuid.uuid1()))
         self.content = "test_book_intro_{}".format(str(uuid.uuid1()))
-        self.tag = "test_tag_{}".format(str(uuid.uuid1()))#random.choice(["小说","励志"])
+        self.tag = "test_tag_{}".format(str(uuid.uuid1()))
         self.author = "test_author_{}".format(str(uuid.uuid1()))
         self.store_id = "test_store_id_{}".format(str(uuid.uuid1()))
 
         yield
 
     def test_search_random(self):
-        assert self.auth.search_author_store(author=self.author) == 200
-        assert self.auth.search_author_store(store_id=self.store_id) == 200
-        assert self.auth.search_author_store(author=self.author, store_id=self.store_id) == 200
+        assert self.auth.search_author_store(author=self.author) == 527
+        assert self.auth.search_author_store(store_id=self.store_id) == 527
+        assert self.auth.search_author_store(author=self.author, store_id=self.store_id) == 527
 
-        assert self.auth.search_bookintro_content(book_intro=self.book_intro) == 200
-        assert self.auth.search_bookintro_content(content=self.content) == 200
-        assert self.auth.search_bookintro_content(book_intro=self.book_intro, store_id=self.store_id) == 200
-        assert self.auth.search_bookintro_content(content=self.content, store_id=self.store_id) == 200
-        assert self.auth.search_bookintro_content(book_intro=self.book_intro, content=self.content, store_id=self.store_id) == 200
+        assert self.auth.search_bookintro_content(book_intro=self.book_intro) == 527
+        assert self.auth.search_bookintro_content(content=self.content) == 527
+        assert self.auth.search_bookintro_content(book_intro=self.book_intro, store_id=self.store_id) == 527
+        assert self.auth.search_bookintro_content(content=self.content, store_id=self.store_id) == 527
+        assert self.auth.search_bookintro_content(book_intro=self.book_intro, content=self.content, store_id=self.store_id) == 527
         
-        assert self.auth.search_tag(self.tag) == 200
-        assert self.auth.search_tag(self.tag, store_id=self.store_id) == 200
+        assert self.auth.search_tag(self.tag) == 527
+        assert self.auth.search_tag(self.tag, store_id=self.store_id) == 527
 
-        assert self.auth.search_title_publisher_isbn(title=self.title) == 200
-        assert self.auth.search_title_publisher_isbn(publisher=self.publisher) == 200
-        assert self.auth.search_title_publisher_isbn(isbn=self.isbn) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, store_id=self.store_id) == 200
-        assert self.auth.search_title_publisher_isbn(publisher=self.publisher, store_id=self.store_id) == 200
-        assert self.auth.search_title_publisher_isbn(isbn=self.isbn, store_id=self.store_id) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, isbn=self.isbn) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher) == 200
-        assert self.auth.search_title_publisher_isbn(publisher=self.publisher, isbn=self.isbn) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, isbn=self.isbn, store_id=self.store_id) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher, store_id=self.store_id) == 200
-        assert self.auth.search_title_publisher_isbn(publisher=self.publisher, isbn=self.isbn, store_id=self.store_id) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher, isbn=self.isbn) == 200
-        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher, isbn=self.isbn, store_id=self.store_id) == 200
+        assert self.auth.search_title_publisher_isbn(title=self.title) == 527
+        assert self.auth.search_title_publisher_isbn(publisher=self.publisher) == 527
+        assert self.auth.search_title_publisher_isbn(isbn=self.isbn) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, store_id=self.store_id) == 527
+        assert self.auth.search_title_publisher_isbn(publisher=self.publisher, store_id=self.store_id) == 527
+        assert self.auth.search_title_publisher_isbn(isbn=self.isbn, store_id=self.store_id) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, isbn=self.isbn) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher) == 527
+        assert self.auth.search_title_publisher_isbn(publisher=self.publisher, isbn=self.isbn) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, isbn=self.isbn, store_id=self.store_id) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher, store_id=self.store_id) == 527
+        assert self.auth.search_title_publisher_isbn(publisher=self.publisher, isbn=self.isbn, store_id=self.store_id) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher, isbn=self.isbn) == 527
+        assert self.auth.search_title_publisher_isbn(title=self.title, publisher=self.publisher, isbn=self.isbn, store_id=self.store_id) == 527
 
     def test_search_fixed(self):
-        title=["早年毛泽东","人类本性与社会秩序","垃圾之歌"]
-        publisher=["台海出版社","人民文学出版社",""]
-        author=["(美)查尔斯.霍顿.库利","李锐","余秋雨"]
-        isbn=["9787508016450","9787500424581","9787205019648"]
-        book_intro=["方法","反映","角度"]
-        content=["暗示和选择","湖南第一师范","见贤思齐"]
-        tags=["人类学","政治学","哥特"]
-        self.store_id = "store_s_1_2_38d34e0a-76c0-11ed-8476-1cbfc037e342" # 某一个店铺
-        self.title = random.choice(title)
-        self.publisher = random.choice(publisher)
-        self.author = random.choice(author)
-        self.isbn = random.choice(isbn)
+        self.title="早年毛泽东"
+        self.publisher="辽宁人民出版社"
+        self.author="李锐"
+        self.isbn="9787205019648"
+        book_intro=["方法","反映","角度"] # 12没有
         self.book_intro = random.choice(book_intro)
-        self.content = random.choice(content)
+        self.content=["心忧天下","湖南第一师范","党的建设"]
+        tags=["darkwave","人物传记","哥特"]
         self.tag = random.choice(tags)
+        self.store_id = "store_s_1_1_09eb7403-76cf-11ed-a512-1cbfc037e342" # 某一个店铺
 
         assert self.auth.search_author_store(author=self.author) == 200
         assert self.auth.search_author_store(store_id=self.store_id) == 200
         assert self.auth.search_author_store(author=self.author, store_id=self.store_id) == 200
 
-        assert self.auth.search_bookintro_content(book_intro=self.book_intro) == 200
+        assert self.auth.search_bookintro_content(book_intro=self.book_intro) == 527
         assert self.auth.search_bookintro_content(content=self.content) == 200
-        assert self.auth.search_bookintro_content(book_intro=self.book_intro, store_id=self.store_id) == 200
+        assert self.auth.search_bookintro_content(book_intro=self.book_intro, store_id=self.store_id) == 527 #
         assert self.auth.search_bookintro_content(content=self.content, store_id=self.store_id) == 200
-        assert self.auth.search_bookintro_content(book_intro=self.book_intro, content=self.content, store_id=self.store_id) == 200
+        assert self.auth.search_bookintro_content(book_intro=self.book_intro, content=self.content, store_id=self.store_id) == 527
         
         assert self.auth.search_tag(self.tag) == 200
         assert self.auth.search_tag(self.tag, store_id=self.store_id) == 200
